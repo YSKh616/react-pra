361f9faee101824cd4b1d034de490c89dfcba029
YSKh616 <hibino616@gmail.com>
2022-05-15 22:17:59 +0900

    add task add func

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
+import { useState } from "react";
+
 function App() {
+  const [text, setText] = useState("");
+  const [tasks, setTasks] = useState([]);
+
   const addTask = (e) => {
     e.preventDefault();
     console.log("add task");
+    let copyTasks = [...tasks];
+    copyTasks.push({ name: text, isCompleted: false });
+    setTasks(copyTasks);
+    setText("");
   };
+
+  const handleNewTask = (e) => {
+    setText(e.target.value);
+  };
+
   return (
     <>
       <h1>TODO LIST</h1>
       <form onSubmit={addTask}>
         <label htmlFor="task-name">タスク名:</label>
-        <input type="text" id="task-name" />
+        <input
+          type="text"
+          id="task-name"
+          value={text}
+          onChange={handleNewTask}
+        />
         <button type="submit">Add</button>
       </form>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>{task.name}</li>
+        ))}
+      </ul>
     </>
   );
 }
