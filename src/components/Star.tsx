fbf31bd860d2b35d4908fa3bd7394ac97c7ec633
YSKh616 <hibino616@gmail.com>
2022-05-19 23:47:07 +0900

    fix stars

--- src/components/Star.tsx
+++ src/components/Star.tsx
@@ -3,11 +3,13 @@ import { FaStar } from "react-icons/fa";
 const Star = () => {
   return (
     <>
-      <FaStar color="red"></FaStar>
-      <FaStar color="red"></FaStar>
-      <FaStar color="red"></FaStar>
-      <FaStar color="gray"></FaStar>
-      <FaStar color="gray"></FaStar>
+      {[
+        <FaStar color="red"></FaStar>,
+        <FaStar color="red"></FaStar>,
+        <FaStar color="red"></FaStar>,
+        <FaStar color="gray"></FaStar>,
+        <FaStar color="gray"></FaStar>,
+      ]}
     </>
   );
 };
