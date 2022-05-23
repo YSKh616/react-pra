324517e772fc8cb74a60f91b000c34d5a61634e3
YSKh616 <hibino616@gmail.com>
2022-05-23 21:49:24 +0900

    星の色を引数で指定

--- src/components/Star.tsx
+++ src/components/Star.tsx
@@ -1,16 +1,27 @@
 import { FaStar } from "react-icons/fa";
+import { useState } from "react";
 
 const Star = ({ selected = false }) => {
   return <FaStar color={selected ? "red" : "grey"} />;
 };
 
-const stars = (): JSX.Element[] => {
+const stars = (selectedStars: number): JSX.Element => {
   const totalStars = 5;
-  return [...Array(totalStars)].map((_, i) => <Star key={i} />);
+  return (
+    <>
+      {[...Array(totalStars)].map((_, i) => (
+        <Star key={i} selected={selectedStars > i} />
+      ))}
+      <p>
+        {selectedStars} of {totalStars} stars
+      </p>
+    </>
+  );
 };
 
 const StarRating = (): JSX.Element => {
-  return <div>{stars()}</div>;
+  const [selectedStars] = useState(4);
+  return <div>{stars(selectedStars)}</div>;
 };
 
 export default StarRating;
