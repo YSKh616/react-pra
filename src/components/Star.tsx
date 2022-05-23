a8ba616d6e37865cdc58aca1fec4811ea8353c9e
YSKh616 <hibino616@gmail.com>
2022-05-23 22:00:54 +0900

    星をクリックして数を指定できるようにする

--- src/components/Star.tsx
+++ src/components/Star.tsx
@@ -1,16 +1,20 @@
 import { FaStar } from "react-icons/fa";
 import { useState } from "react";
 
-const Star = ({ selected = false }) => {
-  return <FaStar color={selected ? "red" : "grey"} />;
+const Star = ({ selected = false, onSelect = (f: any) => f }) => {
+  return <FaStar color={selected ? "red" : "grey"} onClick={onSelect} />;
 };
 
-const stars = (selectedStars: number): JSX.Element => {
+const stars = (selectedStars: number, onSelect: any): JSX.Element => {
   const totalStars = 5;
   return (
     <>
       {[...Array(totalStars)].map((_, i) => (
-        <Star key={i} selected={selectedStars > i} />
+        <Star
+          key={i}
+          selected={selectedStars > i}
+          onSelect={() => onSelect(i)}
+        />
       ))}
       <p>
         {selectedStars} of {totalStars} stars
@@ -20,8 +24,11 @@ const stars = (selectedStars: number): JSX.Element => {
 };
 
 const StarRating = (): JSX.Element => {
-  const [selectedStars] = useState(4);
-  return <div>{stars(selectedStars)}</div>;
+  const [selectedStars, setSelectedStars] = useState(0);
+  const updateStars = (i: number) => {
+    setSelectedStars(i + 1);
+  };
+  return <div>{stars(selectedStars, updateStars)}</div>;
 };
 
 export default StarRating;
